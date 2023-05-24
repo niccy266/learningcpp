0fba23df309b3f668c0d934d340882974e5efa11
Nicolas Concha <niccy266@gmail.com>
2023-05-24 19:46:18 -0400

    added loading text but not functioning

--- PathTracer/render_image/main.cpp
+++ PathTracer/render_image/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char const *argv[])
    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
    for (int j = image_height-1; j >= 0; --j) {
+      std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
       for (int i = 0; i < image_width; ++i) {
          auto r = double(i) / (image_width-1);
          auto g = double(j) / (image_height-1);
@@ -26,6 +27,7 @@ int main(int argc, char const *argv[])
       }
    }
    
+   std::cerr << "\nDone.\n";
 
    return 0;
 }
